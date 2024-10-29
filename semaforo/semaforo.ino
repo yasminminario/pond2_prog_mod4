ce7de50da4edf5c9335d96777bcf8d5223a42b4e
yasminminario <yasmin.minario@sou.inteli.edu.br>
2024-10-29 11:24:46 -0300

    corrige semaforo

--- semaforo/semaforo.ino
+++ semaforo/semaforo.ino
@@ -1,13 +1,14 @@
 const int red = 23;
 const int yellow = 22;
 const int green = 32;
+const int buzzer = 5;
 
 void setup() {
   // inicializa os pins 23, 22 e 32 como output
   pinMode(red, OUTPUT);
   pinMode(yellow, OUTPUT);
   pinMode(green, OUTPUT);
-  
+  pinMode(buzzer, OUTPUT);
 }
 
 void loop() {
@@ -17,9 +18,11 @@ void loop() {
   digitalWrite(yellow, HIGH);  // liga a luz amarela
   delay(2000);                      // espera 2 segundos
   digitalWrite(yellow, LOW);   // desliga a LED mudando o estado para LOW
+  tone(buzzer, 1000);
   digitalWrite(green, HIGH);  // liga a luz verde
   delay(4000);                      // espera 4 segundos
   digitalWrite(green, LOW);   // desliga a LED mudando o estado para LOW
+  noTone(buzzer);
   digitalWrite(yellow, HIGH);  // liga a luz amarela de novo
   delay(2000);                      // espera 2 segundos
   digitalWrite(yellow, LOW);   // desliga a LED mudando o estado para LOW
